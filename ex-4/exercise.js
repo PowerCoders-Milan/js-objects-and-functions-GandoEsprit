c3826f8928e6c2c201cddbc040345367106bcf5f
gando diallo <gandoesprit@gmail.com>
2021-02-26 15:17:22 +0100

    Solution to ex-4

--- ex-4/exercise.js
+++ ex-4/exercise.js
@@ -1,3 +1,24 @@
-// write your JS here
-// remember to always test in the browser
-// to see if everything works correctly
+function calculateAge(dateWasBorn, dateNow){
+
+    if(dateWasBorn.getMonth() == dateNow.getMonth()){
+        return dateNow.getFullYear() - dateWasBorn.getFullYear();
+    }
+    else if(dateWasBorn.getMonth() < dateNow.getMonth()){
+        return dateNow.getFullYear() - dateWasBorn.getFullYear() - 1;
+    }
+    else{
+        return dateNow.getFullYear() - dateWasBorn.getFullYear();
+    }
+}
+
+var dateWasBorn = new Date(2000, 0, 26);
+
+var age = calculateAge(dateWasBorn, new Date());
+
+window.alert('You have ' + age + ' year old');
+
+dateWasBorn = new Date(2000, 1, 26);
+
+age = calculateAge(dateWasBorn, new Date());
+
+window.alert('You have ' + age + ' year old');
