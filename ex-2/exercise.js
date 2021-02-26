85370e0386386c23a89544c433c1f0635b858e0a
gando diallo <gandoesprit@gmail.com>
2021-02-26 11:41:25 +0100

    Solution to ex-2

--- ex-2/exercise.js
+++ ex-2/exercise.js
@@ -1,3 +1,17 @@
-// write your JS here
-// remember to always test in the browser
-// to see if everything works correctly
+
+function tellFortune(numberOfChildren, partnerName, geographicLocation, jobTitle){
+    this.numberOfChildren = numberOfChildren;
+    this.partnerName = partnerName;
+    this.geographicLocation = geographicLocation;
+    this.jobTitle = jobTitle;
+}
+
+var numberOfChildren = prompt('How many children you have?');
+var partnerName = prompt('What is the of your partner?');
+var geographicLocation = prompt('Who do you live');
+var jobTitle = prompt('What is your job');
+
+var info = new tellFortune(numberOfChildren, partnerName, geographicLocation, jobTitle);
+
+window.alert('You will be a ' + info.jobTitle + ' in ' + info.geographicLocation +
+', and married to ' + info.partnerName + ' with ' + info.numberOfChildren + ' kids.');
