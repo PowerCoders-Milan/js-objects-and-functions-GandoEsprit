70e66a7d6204c039156e95e94e0941808f1527ef
gando diallo <gandoesprit@gmail.com>
2021-02-26 11:19:59 +0100

    Solution to ex-1

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,3 +1,11 @@
-// write your JS here
-// remember to always test in the browser
-// to see if everything works correctly
+
+function injectStory(noun, verb, adverb, adjective){
+    window.alert('Name: ' + noun + ' Verb: ' + verb + ' Adverb: ' + adverb + ' adjective: ' + adjective);
+}
+
+var noun = prompt('Put a name');
+var verb = prompt('Put a verb');
+var adverb = prompt('Put a adverb');
+var adjective = prompt('Put a adjective');
+
+injectStory(noun, verb, adverb, adjective);
