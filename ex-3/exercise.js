e373c12b4fc4d42470ecac423dfcd9872cb164a3
gando diallo <gandoesprit@gmail.com>
2021-02-26 12:24:00 +0100

    Solution to ex-3

--- ex-3/exercise.js
+++ ex-3/exercise.js
@@ -1,3 +1,33 @@
-// write your JS here
-// remember to always test in the browser
-// to see if everything works correctly
+var choices = ['Person', 'animal', 'Country'];
+
+window.alert('My #1 choice is ' + choices[0]);
+window.alert('My #2 choice is ' + choices[1]);
+window.alert('My #3 choice is ' + choices[2]);
+
+var person = {
+    name : 'Gando',
+    age : 31,
+    nationality : 'Guinea',
+    profession : 'Programmer'
+}
+
+window.alert('Name: ' + person.name + ', Age: ' + person.age + ', Nationality: ' 
++ person.nationality + ', Profession: ' + person.profession);
+
+var animal = {
+    type : 'herbivorous',
+    kind : 'Cat',
+    color : 'Black'
+}
+
+window.alert('Animal Type: ' + animal.type + ', Animal kind: ' + animal.kind +
+', Animal color: ' + animal.color);
+
+var country = {
+    name : 'Guinea',
+    continent : 'Africa',
+    devise : 'Work Justice solidarity'
+}
+
+window.alert('Country name: ' + country.name + ', Continent: ' + country.continent +
+', Devise: ' + country.devise);
