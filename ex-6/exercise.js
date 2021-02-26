e14794f17da85063139730427298c4fc5dea00f6
gando diallo <gandoesprit@gmail.com>
2021-02-26 16:57:49 +0100

    Solution to ex-6

--- ex-6/exercise.js
+++ ex-6/exercise.js
@@ -1,3 +1,20 @@
-// write your JS here
-// remember to always test in the browser
-// to see if everything works correctly
+
+function greaterNum(number1, number2){
+    if(number1 > number2){
+        window.alert('The greater number of ' + number1 + ' and ' + number2 +
+        ' is ' + number1 + '.');
+    }
+    else if(number1 < number2){
+        window.alert('The greater number of ' + number1 + ' and ' + number2 +
+        ' is ' + number2 + '.');
+    }
+    else{
+        window.alert('The number are equal');
+    }
+}
+
+greaterNum(10, 15);
+greaterNum(20, 15);
+greaterNum(1, 6);
+greaterNum(100, 150);
+greaterNum(15, 15);
